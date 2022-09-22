5fda200c422bab37eab6666ec55ab22dc85e2cc9
King <kinge2@lri-108705.cc.ad.cchs.net>
2022-09-21 21:34:25 -0400

    tweaking msw estimation

--- msw_grid_random_data.py
+++ msw_grid_random_data.py
@@ -1,7 +1,9 @@
+#%%
 from fears.population import Population
 from fears.utils import plotter, dir_manager
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 np.random.seed(109)
 drug_conc_range = [-3,5]
@@ -15,6 +17,7 @@ p = Population(fitness_data='random',
 p.drugless_rates = [1.28949852, 1.14399848, 1.22802236, 0.93619847]
 p.ic50 = [-0.49205992, 1.76224515,  1.39341393,  2.84653598]
 
+#%%
 fig,ax = plt.subplots() 
 
 genotypes = np.array([0,1,2,3])
@@ -27,6 +30,7 @@ ax = plotter.msw_grid(p,genotypes,ax=ax,
 ax.set_xlabel('Drug concentration ($\mathrm{\mu}$g/mL)',fontsize=14)
 fig.savefig('figures/2_allele_grid.pdf')
 
+#%%
 # fig2 = plotter.plot_msw(p,2,ncols=1,figsize=(2.5,4))
 fig2, ax = plotter.plot_msw(p,0,ncols=2,figsize=(6,2))
 
@@ -42,3 +46,4 @@ ax.spines['right'].set_visible(False)
 ax.spines['top'].set_visible(False)
 ax.legend(loc='best',frameon=False,fontsize=14)
 fig3.savefig('figures/random_seascape.pdf')
+# %%
